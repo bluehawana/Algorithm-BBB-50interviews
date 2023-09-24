512181137bbaac06157c88c860fc700afc0ca66c
Hongzhi Li <bluehawana@gmail.com>
2023-09-24 08:32:48 +0200

    second interview question, done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,6 +22,8 @@ median(arr1, arr2) = 3.5
         System.out.println(java.util.Arrays.toString(arr3));
 
         System.out.println("Median of arrays is : " + findMedian(arr3));
+
+        Knapsack();
     }
     private static double findMedian(int[] arr3) {
         int length= arr3.length;
@@ -33,4 +35,32 @@ median(arr1, arr2) = 3.5
         }
 
         }
+
+        /*0-1 Knapsack
+● Question: Given a list of items with values and weights, as well as a max weight,
+find the maximum value you can generate from items where the sum of the
+weights is less than the max.
+● Eg.
+items = {(w:1, v:6), (w:2, v:10), (w:3, v:12)}
+maxWeight = 5
+knapsack(items, maxWeight) = 22
+         */
+    private static void Knapsack(){
+        int[] weight = {1,2,3};
+        int [] value = {6,10,12};
+        int maxWeight = 5;
+        int [] [] dp = new int[weight.length+1][maxWeight+1];
+        for(int i=1;i<=weight.length;i++){
+            for(int j=1;j<=maxWeight;j++){
+                if(weight[i-1]>j){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i-1][j],value[i-1]+dp[i-1][j-weight[i-1]]);
+                }
+            }
+        }
+        System.out.println("Maximum value for knapsack with weight " + maxWeight + " is: " + dp[weight.length][maxWeight]);
+
+    }
     }
